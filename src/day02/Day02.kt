148c2383285c1344545f30754480e71a180285c1
Francis Duvivier <francisduvivier@gmail.com>
2025-12-02 08:22:39 +0100

    Solve day2 part 1 naively
    # Conflicts:
    #	.gitignore

---
+++ src/day02/Day02.kt
@@ -0,0 +1,46 @@
+package day02
+
+import util.prcp
+import util.readInput
+
+
+
+fun main() {
+    fun part1(input: List<String>): Long {
+        val allInvalids = mutableListOf<Long>()
+        for(line in input) {
+            val (a,b) = line.split("-").map { it.toLong() }
+            val invalids: List<Long> = findInvalidsInRange(a, b)
+            allInvalids.addAll(invalids)
+        }
+        return allInvalids.sum()
+    }
+
+    fun part2(input: List<String>): Long {
+        TODO("Not yet implemented")
+    }
+
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02.test")
+    check(part1(testInput) == 1227775554L)
+    val input = readInput("Day02")
+    prcp(part1(input))
+
+
+//    check(part2(testInput) == 900)
+//    prcp(part2(input))
+}
+
+fun findInvalidsInRange(a: Long, b: Long): List<Long> {
+    return findInvalidsInRangeNaive(a, b)
+}
+
+fun findInvalidsInRangeNaive(a: Long, b: Long): List<Long> {
+    val numbers = a..b
+    return numbers.filter { isInvalid(it) }
+}
+
+fun isInvalid(it: Long): Boolean {
+    return it.toString().matches(Regex("^(.*)\\1$"))
+}
---
+++ src/util/Positions.kt
@@ -0,0 +1,70 @@
+package util
+
+import kotlin.math.absoluteValue
+
+
+enum class Rotation(val xMult: Int, val yMult: Int, val zMult: Int = 1) {
+    R1(1, 1), R2(1, -1), R3(-1, 1), R4(-1, -1),
+    R5(1, 1, -1), R6(1, -1, -1), R7(-1, 1, -1), R8(-1, -1, -1);
+
+    fun mutatePos(pos: Pos3D): Pos3D {
+        return Pos3D(pos.x * xMult, pos.y * yMult, pos.z * zMult)
+    }
+}
+
+enum class Facing(val mutator: (Pos3D) -> Pos3D) {
+    F1({ Pos3D(it.x, it.y, it.z) }),
+    F2({ Pos3D(it.x, it.z, it.y) }),
+    F3({ Pos3D(it.z, it.x, it.y) }),
+    F4({ Pos3D(it.z, it.y, it.x) }),
+    F5({ Pos3D(it.y, it.x, it.z) }),
+    F6({ Pos3D(it.y, it.z, it.x) });
+
+    fun mutatePos(pos: Pos3D): Pos3D {
+        return mutator(pos)
+    }
+}
+
+class Orientation(val direction: Facing, val rotation: Rotation) {
+    fun mutatePos(pos3D: Pos3D): Pos3D {
+        return rotation.mutatePos(direction.mutatePos(pos3D))
+    }
+}
+
+class Pos3D(val x: Int, val y: Int, val z: Int) {
+    constructor(list: List<Int>) : this(list[0], list[1], list[2])
+
+    val all = listOf(x, y, z)
+    override fun toString(): String {
+        return all.joinToString(",")
+    }
+
+    fun diff(other: Pos3D): Pos3D {
+        return Pos3D(all.mapIndexed { index, it -> it - other.all[index] })
+    }
+
+    fun orient(orientation: Orientation): Pos3D {
+        return orientation.mutatePos(this);
+    }
+
+    fun add(other: Pos3D): Pos3D {
+        return Pos3D(all.mapIndexed { index, it -> it + other.all[index] })
+    }
+
+    override fun hashCode(): Int {
+        return this.toString().hashCode()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        try {
+            val otherPos = other as Pos3D
+            return otherPos.diff(this).all.all { it == 0 }
+        } catch (e: Error) {
+            return false
+        }
+    }
+
+    fun manHattan(): Int {
+        return all.map { it.absoluteValue }.sum()
+    }
+}
