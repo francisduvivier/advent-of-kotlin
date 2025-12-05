b73acb67a9f3b1812ca26b0659e4c2a23770238c
Francis Duvivier <francisduvivier@gmail.com>
2025-12-05 06:55:29 +0100

    Solve Day5 part 1

---
+++ src/Day05.kt
@@ -0,0 +1,70 @@
+import util.prcp
+import util.readInput
+
+
+fun main() {
+    fun part1(input: List<String>): Long {
+        val (freshRanges, ingredients) = parseInput(input)
+        return ingredients.count { isFresh(it, freshRanges) }.toLong()
+    }
+
+    fun part2(input: List<String>): Long {
+        check(countNewRanges(1L..3, listOf(1L..3)) == 0L)
+        check(countNewRanges(1L..3, listOf(1L..2)) == 1L)
+        check(countNewRanges(1L..3, listOf(2L..3)) == 1L)
+        check(countNewRanges(1L..2, listOf(2L..3)) == 1L)
+        check(countNewRanges(3L..4, listOf(2L..3)) == 1L)
+        check(countNewRanges(3L..3, listOf(2L..3)) == 0L)
+
+        val (freshRanges, _) = parseInput(input)
+        val doneRanges = mutableListOf<LongRange>()
+
+        return freshRanges.sumOf {
+            val added = countNewRanges(it, doneRanges)
+            doneRanges.add(it)
+            added
+        }
+    }
+
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day05.test")
+    check(part1(testInput) == 3L)
+    val input = readInput("Day05")
+    prcp(part1(input))
+
+
+    check(part2(testInput) == 14L)
+    check(part2(testInput+testInput) == 14L)
+    prcp(part2(input))
+}
+
+fun countNewRanges(it: LongRange, doneRanges: List<LongRange>): Long {
+    var start = it.start
+    var endInclusive = it.endInclusive
+    for (doneRange in doneRanges) {
+        if (doneRange.contains(start)) {
+            start = (doneRange.endInclusive + 1).coerceAtLeast(start)
+        }
+        if (doneRange.contains(endInclusive)) {
+            endInclusive = (doneRange.start - 1).coerceAtMost(endInclusive)
+        }
+    }
+    return 0L.coerceAtLeast(endInclusive + 1 - start)
+}
+
+
+fun isFresh(ingredient: Long, freshRanges: List<LongRange>): Boolean {
+    return freshRanges.any { it.contains(ingredient) }
+}
+
+fun parseInput(input: List<String>): Pair<List<LongRange>, List<Long>> {
+    val ranges = input.filter { it.contains("-") }
+        .map {
+            val rangeParts = it.split("-").map { it.toLong() }
+            check(rangeParts[0] <= rangeParts[1])
+            LongRange(rangeParts[0], rangeParts[1])
+        }
+    val ingredients = input.filter { it.matches(Regex("""^\d+$""")) }.map { it.toLong() }
+    return Pair(ranges, ingredients)
+}
