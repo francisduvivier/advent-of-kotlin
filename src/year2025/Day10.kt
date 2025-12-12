d557290e5d58b3b9107d34cd9e82e8bb52aeb3c9
Francis Duvivier <francisduvivier@gmail.com>
2025-12-12 20:05:46 +0100

    WIP day10 part 2

--- src/year2025/Day10.kt
+++ src/year2025/Day10.kt
@@ -6,12 +6,103 @@ import readInput
 
 fun main() {
     fun part1(input: List<String>): Long {
+        fun solveLine(line: String): Long {
+            // line example: [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+            val matchGroups = Regex("""\[([.#]+)] (.*) \{.*""").matchEntire(line)?.groups!!
+            val wantedChars = matchGroups[1]!!.value.toCharArray().map { if (it == '#') 1 else 0 }
+            println(wantedChars)
+            val components: List<List<Int>> =
+                matchGroups[2]!!.value.split(" ")
+                    .map { comp -> comp.slice(1..<comp.lastIndex).split(',').map { it.toInt() } }
+
+            val state: List<Int> = wantedChars.map { 0 }
+            val nbCoeffs = components.size
+
+            fun checkSolutionsRec(
+                components: List<List<Int>>,
+                state: List<Int>,
+                trySize: Int,
+            ): Boolean {
+                if (state == wantedChars) {
+                    return true
+                }
+                if (trySize == 0) {
+                    return false
+                }
+                for (comp in components) {
+                    val newState = pushComp(comp, state)
+                    val checkSolutionsRec = checkSolutionsRec(
+                        components.slice(1 until components.size),
+                        newState,
+                        trySize - 1
+                    )
+                    if (checkSolutionsRec) {
+                        return true
+                    }
+                }
+                return false
+            }
+
+            for (trySize in 1..nbCoeffs) {
+                if (checkSolutionsRec(components, state, trySize)) {
+                    return trySize.toLong()
+                }
+            }
+            throw Exception("No solution found")
+        }
+
         // min sum(X) where X *(dot product) S = WANTED
         // [1,0,1] = x1 * (0,1,1)  + x2 * (1,1,0) * x3 * (0,1,0) + x4 (1,0,0) => x1=1, x2=1 =>X = (1,1,0,0)
         return input.map { solveLine(it) }.sum()
     }
 
     fun part2(input: List<String>): Long {
+        fun solveLine(line: String): Long {
+            // line example: [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+            val matchGroups = Regex("""\[([.#]+)] (.*) \{(.*)}""").matchEntire(line)?.groups!!
+            val components: List<List<Int>> =
+                matchGroups[2]!!.value.split(" ")
+                    .map { comp -> comp.slice(1..<comp.lastIndex).split(',').map { it.toInt() } }
+            val wantedNumbers: List<List<Int>> =
+                matchGroups[3]!!.value.split(" ")
+                    .map { comp -> comp.slice(1..<comp.lastIndex).split(',').map { it.toInt() } }
+
+            val state: List<Int> = wantedNumbers.map { 0 }
+            val nbCoeffs = components.size
+
+            fun checkSolutionsRec(
+                components: List<List<Int>>,
+                state: List<Int>,
+                trySize: Int,
+            ): Boolean {
+                if (state == wantedNumbers) {
+                    return true
+                }
+                if (trySize == 0) {
+                    return false
+                }
+                for (comp in components) {
+                    val newState = pushComp(comp, state)
+                    val checkSolutionsRec = checkSolutionsRec(
+                        components.slice(1 until components.size),
+                        newState,
+                        trySize - 1
+                    )
+                    if (checkSolutionsRec) {
+                        return true
+                    }
+                }
+                return false
+            }
+
+            for (trySize in 1..nbCoeffs) {
+                if (checkSolutionsRec(components, state, trySize)) {
+                    return trySize.toLong()
+                }
+            }
+            throw Exception("No solution found")
+        }
+
         return 0
     }
 
@@ -24,50 +115,6 @@ fun main() {
     prcp(part1(input))
     checkEquals(part2(testInput), 0)
     prcp(part2(input))
-}
-
-fun solveLine(line: String): Long {
-    // line example: [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
-    val matchGroups = Regex("""\[([.#]+)] (.*) \{.*""").matchEntire(line)?.groups!!
-    val wantedChars = matchGroups[1]!!.value.toCharArray().map { if (it == '#') 1 else 0 }
-    println(wantedChars)
-    val components: List<List<Int>> =
-        matchGroups[2]!!.value.split(" ").map { comp -> comp.slice(1..<comp.lastIndex).split(',').map { it.toInt() } }
-
-    val state: List<Int> = wantedChars.map { 0 }
-    val nbCoeffs = components.size
-    
-    fun checkSolutionsRec(
-        components: List<List<Int>>,
-        state: List<Int>,
-        trySize: Int,
-    ): Boolean {
-        if (state == wantedChars) {
-            return true
-        }
-        if (trySize == 0) {
-            return false
-        }
-        for (comp in components) {
-            val newState = pushComp(comp, state)
-            val checkSolutionsRec = checkSolutionsRec(
-                components.slice(1 until components.size),
-                newState,
-                trySize - 1
-            )
-            if (checkSolutionsRec) {
-                return true
-            }
-        }
-        return false
-    }
-
-    for (trySize in 1..nbCoeffs) {
-        if (checkSolutionsRec(components, state, trySize)) {
-            return trySize.toLong()
-        }
-    }
-    throw Exception("No solution found")
 }
 
 
